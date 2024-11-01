340e4d2323a86dfb51fb2f6564fb1038f2300f6f
AdrianGerman <adrian.german1019@gmail.com>
2024-11-01 08:27:49 -0600

    init backend project

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+const { Pol } = require("pg")
+
+const pool = new Pol({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+})
+
+module.exports = pool
