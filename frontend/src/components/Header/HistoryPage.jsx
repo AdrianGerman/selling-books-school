7b39109ac936c2a82339c3be41eb3be7feb78775
AdrianGerman <adrian.german1019@gmail.com>
2024-11-06 10:42:40 -0600

    We solved the tasks (delete, delete all and order by datetime)

--- frontend/src/components/Header/HistoryPage.jsx
+++ frontend/src/components/Header/HistoryPage.jsx
@@ -5,34 +5,26 @@ const HistoryPage = () => {
   const [search, setSearch] = useState("")
   const [filteredPurchases, setFilteredPurchases] = useState([])
 
-  // TODOS:
-  // 1. Poder organizar no solo las compras por dia si no por hora de creación (nuevo campo en la BD)
-  // 2. Poder borrar ordenes
-  // 3. Poder borrar todas las ordenes (para un reset al final del semestre)
-  // TODO ESTO SON TAREAS EN LAS QUE NECESITO CREAR NUEVOS ENDPOINT EN EL BACKEND
-
   useEffect(() => {
-    const fetchPurchases = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/api/purchase")
-        if (!response.ok) {
-          throw new Error("Error al cargar el historial de compras.")
-        }
-        let data = await response.json()
-
-        data = data.sort(
-          (a, b) => new Date(b.sale_date) - new Date(a.sale_date)
-        )
-
-        setPurchases(data)
-        setFilteredPurchases(data)
-      } catch (error) {
-        console.error("Error al obtener el historial:", error)
-      }
-    }
-
     fetchPurchases()
   }, [])
+
+  const fetchPurchases = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/purchase")
+      if (!response.ok) {
+        throw new Error("Error al cargar el historial de compras.")
+      }
+      let data = await response.json()
+
+      data = data.sort((a, b) => new Date(b.sale_date) - new Date(a.sale_date))
+
+      setPurchases(data)
+      setFilteredPurchases(data)
+    } catch (error) {
+      console.error("Error al obtener el historial:", error)
+    }
+  }
 
   const handleSearchChange = (e) => {
     const searchTerm = e.target.value.toLowerCase()
@@ -57,9 +49,51 @@ const HistoryPage = () => {
     setFilteredPurchases(filtered)
   }
 
+  const handleDeletePurchase = async (id) => {
+    const confirmDelete = window.confirm(
+      "¿Estás seguro de que quieres borrar esta compra?"
+    )
+    if (!confirmDelete) return
+
+    try {
+      await fetch(`http://localhost:3000/api/purchase/${id}`, {
+        method: "DELETE"
+      })
+      fetchPurchases()
+    } catch (error) {
+      console.error("Error al eliminar la compra:", error)
+    }
+  }
+
+  const handleDeleteAllPurchases = async () => {
+    if (
+      window.confirm("¿Estás seguro de que quieres borrar todo el historial?")
+    ) {
+      try {
+        await fetch("http://localhost:3000/api/purchase", {
+          method: "DELETE"
+        })
+        setPurchases([])
+        setFilteredPurchases([])
+      } catch (error) {
+        console.error("Error al eliminar todo el historial:", error)
+      }
+    }
+  }
+
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Historial de Compras</h1>
+    <div className="p-6 lg:w-2/4 w-auto m-auto">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-bold">Historial de Compras</h1>
+        <div className="flex gap-2">
+          <button
+            onClick={handleDeleteAllPurchases}
+            className="bg-red-500 text-white px-4 py-2 rounded"
+          >
+            Borrar todo el historial
+          </button>
+        </div>
+      </div>
       <div className="w-full flex items-center mb-4">
         <input
           type="text"
@@ -89,6 +123,16 @@ const HistoryPage = () => {
                   "0"
                 )}
                 .{String(new Date(purchase.sale_date).getFullYear()).slice(-2)}
+                &nbsp;
+                {String(new Date(purchase.sale_date).getHours()).padStart(
+                  2,
+                  "0"
+                )}
+                :
+                {String(new Date(purchase.sale_date).getMinutes()).padStart(
+                  2,
+                  "0"
+                )}
               </p>
               <p>{purchase.student_name}</p>
               <p>{purchase.student_code}</p>
@@ -107,6 +151,25 @@ const HistoryPage = () => {
                 <p>{purchase.items.length} libros</p>
                 <p>${parseFloat(purchase.total_amount).toFixed(2)} mxn</p>
               </div>
+              <button
+                onClick={() => handleDeletePurchase(purchase.sale_id)}
+                className="text-white px-3 py-1 rounded ml-4 transform transition duration-300 hover:scale-110 hover:text-red-700"
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M4 7l16 0" />
+                  <path d="M10 11l0 6" />
+                  <path d="M14 11l0 6" />
+                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                </svg>
+              </button>
             </div>
           </div>
         ))}
