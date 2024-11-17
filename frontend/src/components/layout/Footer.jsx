13f2723b02484c27a0799d9625125bf4293502b8
AdrianGerman <adrian.german1019@gmail.com>
2024-11-17 09:13:21 -0600

    removed the admin from the footer

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,22 +1,8 @@
-import { Link } from "react-router-dom"
-
 const Footer = () => {
-  const userRole = localStorage.getItem("role")
-
   return (
     <div className="text-center font-bold mb-6 mt-6">
       <section>
-        <h1>
-          Designed with ♥ by AdrianGerman
-          {userRole === "admin" && (
-            <>
-              {" · "}
-              <Link to="/admin" className="cursor-pointer underline">
-                Admin
-              </Link>
-            </>
-          )}
-        </h1>
+        <h1>Designed with ♥ by AdrianGerman</h1>
       </section>
     </div>
   )
